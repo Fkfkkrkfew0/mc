7933782437875bd04050943b3e44cf94e59817db
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-29 21:30:27 +0300

    Update bot (4).js

--- bot (4).js
+++ bot (4).js
@@ -22,25 +22,6 @@ channel.send(`نوفرجبرو  نوفرجبرو  نوفرجبرو   نوفرج�
 
 
 
-        var prefix = "$";
-    client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "spam") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR**');
-
-
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
- 
  
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
